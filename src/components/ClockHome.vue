57e3b8527a16f868d7483a418051f4e86ad9e7b7
Sergey <sergey.chuvayev@spendesk.com>
2022-12-18 16:03:14 +0100

    clock logic (#9)
    * clock logic
    
    * fix clock
    
    Co-authored-by: Constance <constance@MacBook-Pro-de-Constance.local>

--- src/components/ClockHome.vue
+++ src/components/ClockHome.vue
@@ -1,125 +1,67 @@
-<!-- <script setup lang="ts">
-import { defineProps } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 
-
-
-
-  public time: Date = new Date();
-  public secondHandStyle: string = '';
-  public minutesHandStyle: string = '';
-  public hoursHandStyle: string = '';
-  private interval = setInterval(() => {
-    this.refreshTime();
- }, 1000);
-
-
-
-
-  ngOnDestroy(): void {
-    clearInterval(this.interval);
-  }
-
-  refreshTime() {
-    const now = new Date();
-
-    const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
-
-    const secondsDegree = (360 / 60) * seconds + 90;
-    const minutesDegree = (360 / 60) * minutes + 90;
-    const hoursDegree = ((360/12) * hours) + 90 + (minutesDegree/12 - 8);
-
-    this.secondHandStyle = `transform: rotate(${secondsDegree}deg)`;
-    this.minutesHandStyle = `transform: rotate(${minutesDegree}deg)`;
-    this.hoursHandStyle = `transform: rotate(${hoursDegree}deg)`;
-
-  }
-
-  
-  public setDate() {
-
-    const min = now.getMinutes()
-    const minsDegree = ((360/60) * min) + 90;
-    this.minHand.style.transform = `rotate(${minsDegree}deg)`;
-    this.minHand.style.transition = minsDegree === 90 ? 'all 0s' : 'all 0.05s';
-    this.minHand.style['transition-timing-function'] = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
-    this.minHand.style.transform = `rotate(${minsDegree}deg)`;
-
-    const hour = now.getHours();
-    // to give a realistic hour hand (minsDegree/12 - 8) was added to normal hoursDegree.
-    // this serves to ofset the hour hand gradually as the minute hand moves
-    // the -8 counters the +90 offset of the original minsDegree
-    const hoursDegree = ((360/12) * hour) + 90 + (minsDegree/12 - 8);
-    this.hourHand.style.transform = `rotate(${hoursDegree}deg)`;
-    this.hourHand.style.transition = hoursDegree === 90 ? 'all 0s' : 'all 0.05s';
-    this.hourHand.style['transition-timing-function'] = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
-    this.hourHand.style.transform = `rotate(${hoursDegree}deg)`;
-  }
-
-  setInterval(setDate, 1000);
-}
-
+let secondsAngle = ref<number>();
+let minutesAngle = ref<number>();
+let hoursAngle = ref<number>();
+setInterval(() => {
+  const time = new Date();
+  secondsAngle.value = (360 / 60) * time.getSeconds();
+  minutesAngle.value = (360 / 60) * time.getMinutes() + 90;
+  hoursAngle.value = (360 / 12) * time.getHours() + 90;
+}, 1000);
 </script>
 
-
-
-
 <template>
-  <body>
-  <div class="clock">
-    <div class="clock-face">
-      <div [style]="hoursHandStyle" class="hand hour-hand"></div>
-      <div [style]="minutesHandStyle" class="hand min-hand"></div>
-      <div [style]="secondHandStyle" class="hand second-hand"></div>
-    </div>
+  <div class="watch">
+    <div
+      :style="{ transform: `rotate(${secondsAngle}deg)` }"
+      class="seconds arrow"
+    />
+    <div
+      :style="{ transform: `rotate(${minutesAngle}deg)` }"
+      class="minutes arrow"
+    />
+    <div
+      :style="{ transform: `rotate(${hoursAngle}deg)` }"
+      class="hours arrow"
+    />
   </div>
-</body>
 </template>
 
 <style scoped>
-body {
-  background: transparent;
-  background-color: transparent;
-}
-.clock {
-  border-radius: 50%;
-  display: flex;
-  position: relative;
-}
-
-.clock-face {
+.watch {
   width: 120px;
   height: 120px;
-  border-radius: 50%;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  background: url('https://github.com/umarcbs/AnalogCLock/blob/master/clock-face2.png?raw=true');
+  position: relative;
+  display: flex;
+  justify-content: center;
+  background: url("https://github.com/umarcbs/AnalogCLock/blob/master/clock-face2.png?raw=true");
   background-size: 100% 100%;
 }
-
-.hand {
+.arrow {
   width: 35%;
-  height: 6px;
-  background-color: black;
+  height: 5px;
+  background-color: var(--color-dark);
   position: absolute;
   border-radius: 2px;
   top: 49.5%;
   left: 17.5%;
   transform-origin: 95%;
   transform: rotate(90deg);
-  transition: all .1s;
+  transition: all 0.1s;
   transition-timing-function: cubic-bezier(0.1, 2.7, 0.58, 1);
 }
-
-.second-hand {
-  height: 3px;
+.seconds {
+  height: 1px;
 }
-
-.hour-hand {
+.minutes {
+  height: 2px;
+}
+.hours {
+  height: 4px;
   width: 20%;
   left: 31.8%;
   top: 50%;
 }
-</style> -->
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import ArticleArea from "../components/ArticleArea.vue";
+import ClockHomeVue from "@/components/ClockHome.vue";
+
+let secondsAngle = ref<number>();
+let minutesAngle = ref<number>();
+let hoursAngle = ref<number>();
+setInterval(() => {
+  const time = new Date();
+  secondsAngle.value = (360 / 60) * time.getSeconds();
+  minutesAngle.value = (360 / 60) * time.getMinutes();
+  hoursAngle.value = (360 / 12) * time.getHours();
+}, 1000);
 </script>
 
 <template>
@@ -12,7 +24,7 @@ import ArticleArea from "../components/ArticleArea.vue";
           <div class="title__text">Amicale du Gabo</div>
           <img src="/delimiter2.svg" alt="delimiter" />
         </div>
-        <!-- <clock class="home__clock"></clock> -->
+        <ClockHomeVue />
         <div class="home__quote">
           <div class="home__quote__text">
             "Le temps s'écoule sans faire de bruit"
@@ -46,25 +58,20 @@ import ArticleArea from "../components/ArticleArea.vue";
 }
 
 .home__title {
-  font-family: 'Milonga', cursive;
+  font-family: "Milonga", cursive;
   font-size: 54px;
   text-align: center;
 }
 
-.home__clock {
-  height: 130px;
-}
-
-.home__quote{
-  font-family: 'Lancelot', cursive;
+.home__quote {
+  font-family: "Lancelot", cursive;
   text-align: center;
   margin-bottom: 30px;
 }
 .home__quote__text {
   font-size: 38px;
-} 
+}
 .home__quote__author {
   font-size: 26px;
 }
-
 </style>
